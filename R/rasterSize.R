64d962345e79f04c3ea11d64bf1b3eef8bc66f5f
zibbini <ziadibbini@gmail.com>
2020-02-15 16:23:12 +0000

    Add documentation memory functions

--- R/rasterSize.R
+++ R/rasterSize.R
@@ -1,5 +1,14 @@
 rasterSize <- function(raster, format = "mb", verbose = TRUE) {
   
+  #' Memory related functions
+  #' 
+  #' @description Calculates the size of a given raster layer. 
+  #' 
+  #' @param raster raster layer to get the size of.
+  #' @param format Memory output format. Choices are "gb" for gigabytes, "mb" for megabytes 
+  #' and "kb" for kilobytes.
+  #' @param verbose Whether to print verbose text. If FALSE, only the numeric value is returned.
+  
   byteSize   <- unlist(strsplit(raster@file@datanotation, split = ""))
   rasterSize <- raster@ncols * raster@nrows * as.numeric(byteSize[4])
   
@@ -20,3 +29,5 @@ rasterSize <- function(raster, format = "mb", verbose = TRUE) {
   }
   
 }
+
+docstring::docstring(rasterSize)
